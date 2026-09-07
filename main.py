f0c092c85893f2713acdae09671e8b582aa32ff0
ManishaBose <manishabose.2001@gmail.com>
2026-09-07 09:38:21 +0530

    Added pydantic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +12,15 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 # from langchain_tavily import TavilySearch
 from tavily import TavilyClient
+
+class Source(BaseModel):
+    """Scheme for a source used by the agent."""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Scheme for agent response with answer and sources"""
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="The list of sources used to generate the answer")
 
 tavily = TavilyClient()
 
@@ -35,7 +47,7 @@ tools = [search]
 
 # tools = [TavilySearch()]
 
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
